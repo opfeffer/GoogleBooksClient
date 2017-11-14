f8c573b8d84539caf606b0022fac57f6a9af2c03
Oli Pfeffer <oliver.pfeffer@gmail.com>
2017-11-13 20:00:20 -0500

    Initial Commit

---
+++ GoogleBooksClient/GoogleBooksClient.h
@@ -0,0 +1,19 @@
+//
+//  GoogleBooksClient.h
+//  GoogleBooksClient
+//
+//  Created by Oliver Pfeffer on 11/13/17.
+//  Copyright © 2017 Astrio, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleBooksClient.
+FOUNDATION_EXPORT double GoogleBooksClientVersionNumber;
+
+//! Project version string for GoogleBooksClient.
+FOUNDATION_EXPORT const unsigned char GoogleBooksClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleBooksClient/PublicHeader.h>
+
+
